4f6a5c0a5dee681130577e931b810f81a547f7af
nsmith677 <nicole.smith677@gmail.com>
2018-01-25 16:54:07 -0500

    Created Project
    Initial Commit

---
+++ DerbyCityDevelopment/DerbyCityDevelopment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DerbyCityDevelopment.Startup))]
+namespace DerbyCityDevelopment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
